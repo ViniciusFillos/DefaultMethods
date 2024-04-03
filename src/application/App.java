97b96c4561b22708c97ec2b7c6baf357aaab8dc4
Vinicius Fillos <vinifillos@gmail.com>
2024-04-03 07:21:47 -0300

    Project completed: multiple inheritance with interfaces

---
+++ src/application/App.java
@@ -0,0 +1,30 @@
+package application;
+
+import service.BrazilInterestService;
+import service.InterestService;
+import service.USAInterestService;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Amount: ");
+        double amount = sc.nextDouble();
+        System.out.print("Months: ");
+        int months = sc.nextInt();
+
+        InterestService is = new BrazilInterestService(2.0);
+        double payment = is.payment(amount, months);
+
+        System.out.println("Payment after " + months + " months:");
+        System.out.printf("%.2f%n", payment);
+
+        sc.close();
+    }
+}
